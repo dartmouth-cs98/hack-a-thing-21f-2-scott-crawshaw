8d516d6f78cfe066be9cbf43de6300a6c76474b3
Scott Crawshaw <scott.crawshaw@gmail.com>
2021-09-29 13:43:42 -0400

    added wifi code

--- Wifi Tester/Wifi Tester/ViewController.swift
+++ Wifi Tester/Wifi Tester/ViewController.swift
@@ -3,9 +3,11 @@
 //  Wifi Tester
 //
 //  Created by Scott Crawshaw on 9/29/21.
-//
+//  code adapted from https://www.ralfebert.de/ios/beginner-tutorials/iphone-app-xcode/
 
 import UIKit
+import Foundation
+import SystemConfiguration.CaptiveNetwork
 
 class ViewController: UIViewController {
     @IBOutlet weak var wifi_button: UIButton!
@@ -18,7 +20,20 @@ class ViewController: UIViewController {
 
 
     @IBAction func refresh(_ sender: Any) {
-        wifi_label.text = "worked"
+        // code adapted from https://blog.appnation.co/how-to-access-wifi-ssid-on-ios-13-using-swift-40c4bba3c81d
+        
+        var bssid: String?
+        bssid = "No BSSID Found"
+        if let interfaces = CNCopySupportedInterfaces() as NSArray? {
+            for interface in interfaces {
+                if let interfaceInfo = CNCopyCurrentNetworkInfo(interface as! CFString) as NSDictionary? {
+                    bssid = interfaceInfo[kCNNetworkInfoKeySSID as String] as? String
+                    break
+                }
+            }
+        }
+        
+        wifi_label.text = bssid
     }
 }
 
